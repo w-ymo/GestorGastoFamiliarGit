e6cad682915baefd2b18baa39770653c6f0a49f6
w-ymo <117979827+w-ymo@users.noreply.github.com>
2024-01-17 12:17:34 +0100

    Subidos mis cosas 1

---
+++ app/src/main/java/com/example/gestorgastofamiliar/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.example.gestorgastofamiliar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gestorgastofamiliar.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
--- app/src/main/java/com/example/gestorgastofamiliar/adapter/RecyclerViewAdapterGasto.kt
+++ app/src/main/java/com/example/gestorgastofamiliar/adapter/RecyclerViewAdapterGasto.kt
@@ -1,48 +1,48 @@
-package com.example.gestorgastofamiliar.adapter
-
-import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import android.widget.TextView
-
-import com.example.gestorgastofamiliar.placeholder.PlaceholderContent.PlaceholderItem
-import com.example.gestorgastofamiliar.databinding.FragmentGastoBinding
-
-/**
- * [RecyclerView.Adapter] that can display a [PlaceholderItem].
- * TODO: Replace the implementation with code for your data type.
- */
-class RecyclerViewAdapterGasto(
-    private val values: List<PlaceholderItem>
-) : RecyclerView.Adapter<RecyclerViewAdapterGasto.ViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-
-        return ViewHolder(
-            FragmentGastoBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
-
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        holder.idView.text = item.id
-        holder.contentView.text = item.content
-    }
-
-    override fun getItemCount(): Int = values.size
-
-    inner class ViewHolder(binding: FragmentGastoBinding) : RecyclerView.ViewHolder(binding.root) {
-        val idView: TextView = binding.itemNumber
-        val contentView: TextView = binding.content
-
-        override fun toString(): String {
-            return super.toString() + " '" + contentView.text + "'"
-        }
-    }
-
-}
+//package com.example.gestorgastofamiliar.adapter
+//
+//import androidx.recyclerview.widget.RecyclerView
+//import android.view.LayoutInflater
+//import android.view.ViewGroup
+//import android.widget.TextView
+//
+//import com.example.gestorgastofamiliar.placeholder.PlaceholderContent.PlaceholderItem
+//import com.example.gestorgastofamiliar.databinding.FragmentGastoBinding
+//
+///**
+// * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+// * TODO: Replace the implementation with code for your data type.
+// */
+//class RecyclerViewAdapterGasto(
+//    private val values: List<PlaceholderItem>
+//) : RecyclerView.Adapter<RecyclerViewAdapterGasto.ViewHolder>() {
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//
+//        return ViewHolder(
+//            FragmentGastoBinding.inflate(
+//                LayoutInflater.from(parent.context),
+//                parent,
+//                false
+//            )
+//        )
+//
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        val item = values[position]
+//        holder.idView.text = item.id
+//        holder.contentView.text = item.content
+//    }
+//
+//    override fun getItemCount(): Int = values.size
+//
+//    inner class ViewHolder(binding: FragmentGastoBinding) : RecyclerView.ViewHolder(binding.root) {
+//        val idView: TextView = binding.itemNumber
+//        val contentView: TextView = binding.content
+//
+//        override fun toString(): String {
+//            return super.toString() + " '" + contentView.text + "'"
+//        }
+//    }
+//
+//}

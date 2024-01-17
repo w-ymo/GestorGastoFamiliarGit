91d6dce79823f176d19547429d2dcda835615a73
Noel Prieto <noel.prieto.uva@gmail.com>
2024-01-17 10:36:40 +0100

    Creacion de proyecto TOMALO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.gestorgastofamiliar"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.gestorgastofamiliar"
